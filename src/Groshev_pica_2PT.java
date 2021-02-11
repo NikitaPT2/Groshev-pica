f9cc18a9fb1ec37e16965d49aef0d86b635c2a8d
Nikita <nikitogka@gmail.com>
2021-02-12 00:54:23 +0200

    2 versija (switch konstrukcija, joptionpane un apturesana

--- src/Groshev_pica_2PT.java
+++ src/Groshev_pica_2PT.java
@@ -1,8 +1,28 @@
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class Groshev_pica_2PT {
 
 	public static void main(String[] args) {
+		 int izvele, apturet = 0;
+		do{
+			izvele = JOptionPane.showConfirmDialog(null, "Vai Jūs grībat nopirkt picu?", "Pica!", JOptionPane.YES_NO_OPTION);
+			switch(izvele){
+			
+			case JOptionPane.YES_OPTION:
 
+			break;
+			
+			case JOptionPane.NO_OPTION:
+
+			break;
+			
+			default:
+				JOptionPane.showMessageDialog(null, "Darbība nepastāv!", "Kļūda!", JOptionPane.ERROR_MESSAGE);
+			break;
+			}
+			
+		}while(apturet!=1);
+	}
 	}
 
-}
