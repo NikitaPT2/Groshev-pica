a6aac2c464f259c2eb2a1c7dcbb66c1477284319
Nikita <nikitogka@gmail.com>
2021-02-12 01:12:19 +0200

    3 vesija (samainiju krasu un nosaukumus pogam)

--- src/Groshev_pica_2PT.java
+++ src/Groshev_pica_2PT.java
@@ -1,11 +1,16 @@
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
 
 public class Groshev_pica_2PT {
 
 	public static void main(String[] args) {
+		 UIManager.put("OptionPane.background",new ColorUIResource(255,191,0));
+		 UIManager.put("Panel.background",new ColorUIResource(255,191,0));
 		 int izvele, apturet = 0;
 		do{
+			UIManager.put("OptionPane.yesButtonText", "Jā");
+			UIManager.put("OptionPane.noButtonText", "Nē, apturēt programmu");
 			izvele = JOptionPane.showConfirmDialog(null, "Vai Jūs grībat nopirkt picu?", "Pica!", JOptionPane.YES_NO_OPTION);
 			switch(izvele){
 			
